44833fa2023f349de39640b991ca4f0ddd6841ef
AYKDM <minhthai23012002@gmail.com>
2023-10-09 13:53:43 +0800

    first commit

---
+++ utils/constant.ts
@@ -0,0 +1,9 @@
+export const configs = {
+    baseURL: "https://backendsocial-tpej.onrender.com/"
+    // baseURL: "http://localhost:5000",
+    // baseURL : "https://server-iccq.onrender.com"
+}
+
+export const colors = {
+    main: "#1877F2"
+}
